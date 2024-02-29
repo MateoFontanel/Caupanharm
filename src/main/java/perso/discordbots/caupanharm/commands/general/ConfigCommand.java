33b7ebb39500d5cad63e3b06b8c1497fa32f224e
Matéo Fontanel <fontanelmateo@gmail.com>
2024-02-29 03:56:21 +0100

    Adding first channels controls

---
+++ src/main/java/perso/discordbots/caupanharm/commands/general/ConfigCommand.java
@@ -0,0 +1,59 @@
+package perso.discordbots.caupanharm.commands.general;
+
+import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.command.ApplicationCommandInteractionOption;
+import discord4j.core.object.command.ApplicationCommandInteractionOptionValue;
+import discord4j.core.object.entity.channel.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import perso.discordbots.caupanharm.commands.SlashCommand;
+import perso.discordbots.caupanharm.controllers.ChannelController;
+import reactor.core.publisher.Mono;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+@Component
+public class ConfigCommand implements SlashCommand {
+    private final static Logger logger = LoggerFactory.getLogger(ConfigCommand.class);
+    @Value("#{'${admin_discord_id}'.split(',')}")
+    ArrayList<String> authorized_ids;
+
+    @Autowired
+    ChannelController channelController;
+
+    @Override
+    public String getName() {
+        return "config";
+    }
+
+    @Override
+    public Mono<Void> handle(ChatInputInteractionEvent event) throws UnsupportedEncodingException {
+        String response = " ";
+
+        if (!authorized_ids.contains(event.getInteraction().getUser().getId().asString())) {
+            response = "**Error: **You do not have permission to execute this command.";
+        } else {
+            switch (event.getOptions().get(0).getName()) {
+                case "setgame":
+                    String game = event.getOption("setgame")
+                            .flatMap(ApplicationCommandInteractionOption::getValue)
+                            .map(ApplicationCommandInteractionOptionValue::asString)
+                            .get();
+                    response = channelController.setChannel(event.getInteraction().getChannelId().asString(), game);
+                    break;
+
+                default:
+                    response = "Internal error";
+            }
+        }
+
+
+        return event.reply().withEphemeral(true).withContent(response);
+
+    }
+
+}
---
+++ src/main/java/perso/discordbots/caupanharm/controllers/ChannelController.java
@@ -0,0 +1,94 @@
+package perso.discordbots.caupanharm.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import perso.discordbots.caupanharm.models.CaupanharmChannel;
+import perso.discordbots.caupanharm.models.CaupanharmTeam;
+import perso.discordbots.caupanharm.models.CaupanharmUser;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.rmi.server.ExportException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+
+@Controller
+public class ChannelController {
+    private final static Logger logger = LoggerFactory.getLogger(ChannelController.class);
+    private final static File channelsFile = new File("./src/main/resources/channels.json");
+
+    private final static HashMap<String, String> games = new HashMap<>();
+    ObjectMapper mapper = new ObjectMapper();
+    HashMap<String, String> channels;
+
+    public ChannelController() throws IOException {
+        games.put("VAL", "VALORANT");
+        games.put("LOL", "League Of Legends");
+        try {
+            channels = mapper.readValue(channelsFile, HashMap.class);
+            logger.info("Retrieved authorized channels list");
+        } catch (Exception e) {
+            channels = new HashMap<String, String>();
+            mapper.writeValue(channelsFile, channels);
+            logger.info("Created the list of authorized channels");
+        }
+    }
+
+    public String setChannel(String channelId, String game) {
+        String channelGame = channels.get(channelId);
+
+        switch (game) {
+            case "VAL":
+            case "LOL":
+                if (!channels.containsKey(channelId)) { // First case, the channel doesn't monitor any game so far
+                    channels.put(channelId, game);
+                    logger.info("Channel "+channelId+" added to "+game+" monitoring");
+                    if (write(channelsFile, channels))
+                        return "Caupanharm will now start listening to " + games.get(game) + " commands in this channel";
+                } else if (!Objects.equals(channels.get(channelId), game)) { // Second case, the channel already monitor another game
+                    channels.replace(channelId, game);
+                    logger.info("Channel "+channelId+" added to "+game+" monitoring");
+                    if (write(channelsFile, channels))
+                        return "Caupanharm will now start listening to " + games.get(game) + " commands in this channel";
+                } else { // Last case, the channel already monitors the given game
+                    return "**Error**: Caupanharm already monitors commands for " + games.get(game) + " in this channel";
+                }
+
+            default: // Case None/NULL
+                if (channels.containsKey(channelId)) {
+                    channels.remove(channelId);
+                    logger.info("Channel "+channelId+" removed from monitoring");
+                    if (write(channelsFile, channels))
+                        return "Caupanharm won't listen to further game commands in this channel";
+                } else {
+                    return "**Error**: Caupanharm already doesn't listen to any game's commands in this channel";
+                }
+        }
+
+        return "Internal error, please contact Herrahan";
+    }
+
+    public boolean getAuthorization(String channelId, String game) {
+        // TODO
+        return false;
+    }
+
+    private boolean write(File file, Object data) {
+        try {
+            mapper.writeValue(file, data);
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
